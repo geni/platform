8410812932f61672a3c7ba2662cff558555bf6b7
Scott Steadman <scott.steadman@geni.com>
2026-04-09 10:10:53 -0700

    update test_my_app.sh

--- test_my_app.sh
+++ test_my_app.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-unset RBENV_VERSION
-export RBENV_ROOT=/opt/rbenv
-export PATH=${RBENV_ROOT}/shims:${PATH}:${RBENV_ROOT}/bin
-
-git gc
-
-bundle config --local build.pg "--with-pg-config=/usr/pgsql-10/bin/pg_config"
 bundle config --local build.sqlite3 "--enable-system-libraries"
 bundle config --local clean true
 bundle config --local path vendor/bundle
 bundle config --local without vscode
 
+# The bundler version can change between branches
 rm -f Gemfile.lock
+
 bundle install
 
 rm -f db/test.sqlite3
