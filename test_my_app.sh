99c9df5683c2d4cebf3bfa5bcb5186ba589a25e2
Scott Steadman <scott.steadman@geni.com>
2026-04-09 10:11:33 -0700

    update test_my_app.sh

--- test_my_app.sh
+++ test_my_app.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#bundle config --local build.pg "--with-pg-config=/usr/pgsql-15/bin/pg_config"
 bundle config --local clean true
 bundle config --local path vendor/bundle
 bundle config --local without vscode
 
+# The bundler version can change between branches
 rm -rf Gemfile.lock vendor/bundle
 bundle install
 
